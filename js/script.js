5989c23f476729d3eaea7762a6463c91118d0131
Andrew T <at100991@student.sgh.waw.pl>
2020-12-18 17:49:22 +0200

    Add the ability see typed numbers in output

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
+document.addEventListener('DOMContentLoaded', (e) => {
+   const operands = document.querySelectorAll('.operand')
+   const output = document.querySelector('.output')
+   operands.forEach(operand => document.addEventListener('click', showOutput))
+   
+})
+
+
 function operate(operator, numberOne, numberTwo) { }
+
+function showOutput(e) {
+   let buttonValue = e.target.textContent;
+   output.value += buttonValue;
+   
+}
