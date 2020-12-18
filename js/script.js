411b38d49dc71ce1dad0dd9abd7f850c47424412
Andrew T <at100991@student.sgh.waw.pl>
2020-12-18 19:10:19 +0200

    Add the ability to clear output,when user click on "AC" button

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
 document.addEventListener('DOMContentLoaded', (e) => {
-   const operands = document.querySelectorAll('.operand')
-   const output = document.querySelector('.output')
-   operands.forEach(operand => document.addEventListener('click', showOutput))
+  const operands = document.querySelectorAll('.operand');
+  const operators = document.querySelectorAll('.operator');
+  const clearButton = document.querySelector('#clear');
+  const output = document.querySelector('.output');
+   operands.forEach((operand) => document.addEventListener('click', showOutput));
+   clearButton.addEventListener('click', clearOutput);
    
-})
+});
 
-
-function operate(operator, numberOne, numberTwo) { }
+function operate(operator, numberOne, numberTwo) {}
 
 /*
 Show output as sum of output.value & button.value 
 */
 function showOutput(e) {
-   if (e.target.className === 'operand') {
-      if (output.value.length < 8) {
-         console.log(output.value.length)
-         let buttonValue = e.target.textContent;
-         output.value += buttonValue;
-      }
-   }
+  if (e.target.className === 'operand') {
+    if (output.value.length < 8) {
+      let buttonValue = e.target.textContent;
+      output.value += buttonValue;
+    }
+  }
 }
 
+function clearOutput() {
+   output.value = '';
+}
